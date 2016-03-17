86b78cadbf5f3b17a991f23ccba3648da4ab40aa
davidpaupst <davidpaupst@kings.edu>
2016-03-16 21:55:28 -0400

    Need to work on documentation, added the message box clear.

--- LabProjectPhase2/src/edu/kings/im/phase2/IMUserInterface.java
+++ LabProjectPhase2/src/edu/kings/im/phase2/IMUserInterface.java
@@ -32,198 +32,184 @@ import edu.kings.im.Message;
 import edu.kings.im.RealIMConnection;
 import edu.kings.im.StatusListener;
 
-/** GUI for NullPointTexting.
+/**
+ * GUI for Instant Messaging.
  * 
  * @author Dave Paupst
  * @author Johnny Collado
  * @version 02/26/2016
  */
-public class IMUserInterface  implements ActionListener {
-	
+public class IMUserInterface implements ActionListener {
+
+	/** Frame. */
 	private JFrame frame;
 	
+	/** Chat box for viewing messages. */
 	private JTextArea chatBox;
 	
+	/** Message box for sending messages. */
 	private JTextField messageBox;
 	
+	/** Button to send messages. */
 	private JButton sendButton;
 	
+	/** Menu containing users. */
 	private JComboBox<String> users;
 	
+	/** Menu bar. */
 	private JMenuBar menuBar;
 	
-	private boolean connected;
-	
-	
+	/** */
 	private JFrame loginFrame;
 	
-	/**Represents the name for all users.*/
+	/** Represents the name for all users. */
 	public final static String BLAST;
 	
 	/** Keeps track of the current user. */
 	private IMConnection user;
-	
+
 	static {
-		 BLAST = "EVERYONE";
+		BLAST = "EVERYONE";
 	}
-	
+
 	/**
 	 * Constructor.
 	 */
 	public IMUserInterface() {
-		//TODO: Implement me.
-		
-		//variable set up
-		connected = false;
-		
-		//Frame set up
+		// Frame set up
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(600, 600));
-		
-		//Setup the chat box.
+
+		// Setup the chat box.
 		chatBox = new JTextArea();
 		chatBox.setLineWrap(true);
-		
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.setViewportView(chatBox);
-		
-		
-		
+
 		frame.add(scrollPane, BorderLayout.CENTER);
-		
-		
-		//Setup message area.
-		
+
+		// Setup message area.
+
 		JPanel messageArea = new JPanel(new GridBagLayout());
 		GridBagConstraints messageConstraints = new GridBagConstraints();
-		
+
 		messageBox = new JTextField();
 		messageBox.setBorder(BorderFactory.createTitledBorder("Enter Message"));
-		
-		
+
 		messageConstraints.fill = GridBagConstraints.BOTH;
 		messageConstraints.gridx = 0;
 		messageConstraints.gridy = 0;
 		messageConstraints.weightx = .7;
 		messageConstraints.weighty = 1;
 		messageArea.add(messageBox, messageConstraints);
-		
-		
-		//Send button & user list.
-		
+
+		// Send button & user list.
+
 		JPanel userSend = new JPanel(new GridLayout(2, 1));
-		
-		
+
 		users = new JComboBox<String>();
 		users.addItem(BLAST);
-		
-		
+
 		sendButton = new JButton("Send");
 		sendButton.addActionListener(this);
-		
+
 		userSend.add(users);
 		userSend.add(sendButton);
-		
+
 		messageConstraints.fill = GridBagConstraints.BOTH;
 		messageConstraints.gridx = 1;
 		messageConstraints.gridy = 0;
 		messageConstraints.weightx = .3;
 		messageConstraints.weighty = 1;
 		messageArea.add(userSend, messageConstraints);
-		
+
 		frame.add(messageArea, BorderLayout.SOUTH);
-		
-		
+
 		menuBar = new JMenuBar();
-		
+
 		JMenu file = new JMenu("File");
 		file.setMnemonic(KeyEvent.VK_F);
 		menuBar.add(file);
-		
+
 		JMenuItem exit = new JMenuItem("Exit");
-		exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, Event.ALT_MASK));
+		exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4,
+				Event.ALT_MASK));
 		exit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				closeWindow();
 			}
 		});
-		
+
 		file.add(exit);
-		
+
 		frame.setJMenuBar(menuBar);
-		
+
 		frame.setVisible(false);
-		
+
 		login();
 	}
-	
+
 	private void login() {
-		//TODO: add Logo.
-		
-		//Login setup
+		// TODO: add Logo.
+
+		// Login setup
 		loginFrame = new JFrame("Login");
 		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		loginFrame.setSize(300, 200);
-		
-		
+
 		JPanel loginPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints loginC = new GridBagConstraints();
-		
+
 		final JTextField name = new JTextField();
 		name.setBorder(BorderFactory.createTitledBorder("Username"));
-
-		
 
 		loginC.fill = GridBagConstraints.HORIZONTAL;
 		loginC.gridx = 0;
 		loginC.gridy = 1;
 		loginC.weightx = .7;
 		loginC.weighty = .8;
-		
+
 		loginPanel.add(name, loginC);
-		
-		//login prompt and logic
-		
+
+		// login prompt and logic
+
 		loginFrame.add(loginPanel, BorderLayout.CENTER);
-		
+
 		JButton loginButton = new JButton("Login");
 		loginButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				String username = name.getText();
 				IMConnection newUser = RealIMConnection.getInstance(username);
 				newUser.addConnectionListener(new IMStatusListener());
-				
-				
+
 				try {
-				newUser.connect();	
-				}
-				catch(IllegalNameException ine) {
+					newUser.connect();
+				} catch (IllegalNameException ine) {
 					JOptionPane.showMessageDialog(frame, ine.getMessage());
 				}
-				
-				
+
 			}
 		});
-		
+
 		loginC.fill = GridBagConstraints.NONE;
 		loginC.gridx = 0;
 		loginC.gridy = 2;
 		loginC.weightx = .7;
 		loginC.weighty = .05;
-		
+
 		loginPanel.add(loginButton, loginC);
-		
+
 		loginFrame.setVisible(true);
-		
+
 	}
-	
+
 	/**
 	 * Potential protected getter for User.
 	 * 
@@ -232,7 +218,7 @@ public class IMUserInterface  implements ActionListener {
 	protected IMConnection getUser() {
 		return user;
 	}
-	
+
 	/**
 	 * Potential protected setter for User.
 	 * 
@@ -241,7 +227,7 @@ public class IMUserInterface  implements ActionListener {
 	protected void setUser(IMConnection daUser) {
 		user = daUser;
 	}
-	
+
 	/**
 	 * Sends a message to specified user.
 	 * 
@@ -249,16 +235,15 @@ public class IMUserInterface  implements ActionListener {
 	 * @param toWhom
 	 */
 	public void sendMessage(String message, String toWhom) {
-		if(toWhom.equals(BLAST)) {
+		if (toWhom.equals(BLAST)) {
 			user.spamEveryone(message);
 		} else {
 			user.sendMessage(message, toWhom);
 		}
 	}
-	
-	
+
 	private void closeWindow() {
-		//TODO: Implement Logout;
+		// TODO: Implement Logout;
 		user.disconnect();
 		System.exit(0);
 	}
@@ -268,29 +253,25 @@ public class IMUserInterface  implements ActionListener {
 		// TODO Implement for send button.
 		String msg = messageBox.getText();
 		String recipient = (String) users.getSelectedItem();
-		if(msg != null && !msg.trim().equals("")) {
+		if (msg != null && !msg.trim().equals("")) {
 			sendMessage(msg, recipient);
 		}
+		messageBox.setText("");
 	}
 
-
-	
-	
-	
-	
 	private class IMStatusListener implements StatusListener {
-		
+
 		@Override
 		public void connectionRejected(RealIMConnection arg0) {
 			// TODO When login false.
 			JOptionPane.showMessageDialog(frame, "This username is taken.");
-			
+
 		}
 
 		@Override
 		public void connectionUpdate(RealIMConnection arg0, boolean connected) {
 			// TODO implement login process.
-			if(connected) {
+			if (connected) {
 				frame.pack();
 				frame.setVisible(true);
 				loginFrame.dispose();
@@ -300,23 +281,24 @@ public class IMUserInterface  implements ActionListener {
 		}
 
 	}
-	
+
 	private class MessageListener implements IMEventListener {
 
 		@Override
 		public void messagesReceived(Iterable<Message> arg0) {
 			// TODO implement add to chatBox
 			for (Message msg : arg0) {
-				String message = String.format("%s\n\t%s\n", msg.getSender(), msg.getText());
+				String message = String.format("%s\n\t%s\n", msg.getSender(),
+						msg.getText());
 				chatBox.append(message);
 			}
-			
-		}		
+
+		}
 
 		@Override
 		public void userOffline(String arg0) {
 			// TODO remove user from users
-			users.removeItem(arg0);		
+			users.removeItem(arg0);
 		}
 
 		@Override
@@ -324,9 +306,7 @@ public class IMUserInterface  implements ActionListener {
 			// TODO add user to users
 			users.addItem(arg0);
 		}
-		
+
 	}
 
 }
-
-
